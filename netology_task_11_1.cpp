27537a36c0705274dccf3ec095bd2f58ec022ebc
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 23:16:35 +0300

    Add files via upload

---
+++ netology_task_11_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string name{};
+    std::string secondName{};
+
+    std::cout << "Enter your name: " << std::endl;
+    std::cin >> name;
+    std::cout << "Enter your second name: " << std::endl;
+    std::cin >> secondName;
+
+    std::cout << "Hello, " << name << " " << secondName << ".";
+}
+
