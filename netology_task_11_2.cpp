078877898349c752d0597caf3363bab91663e0e3
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 23:22:25 +0300

    Add files via upload

---
+++ netology_task_11_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+
+	std::string correctWord{ "Exile" };
+	std::string inputWord{};
+
+	while (inputWord != correctWord) {
+		std::cout << "Try to guess the word~!" << std::endl;
+		std::cin >> inputWord;
+	}
+	std::cout << "Correct! The right word was: " << correctWord;
+	
+
+
+	return 0;
+}
